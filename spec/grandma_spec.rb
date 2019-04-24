57f938bf688655099cfce60f5966cd0ab9730d58
Shemmyla Green <shemmyla@hotmail.com>
2019-04-24 03:46:23 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -2,15 +2,19 @@ describe '#speak_to_grandma' do
   it 'responds with I LOVE YOU GRANDMA!if you say I LOVE YOU GRANDMA!' do
     expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
     if "I LOVE YOU GRANDMA!"
-      put
+      puts "I LOVE YOU TOO PUMPKIN!"
   end
 
   it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
     expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
+    elsif "Hi Nana, how are you?"
+      puts "HUH?! SPEAK UP, SONNY!"
   end
 
   it 'responds with HUH?! SPEAK UP, SONNY! for a second time' do
     expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
+    elsif "Hi!"
+      puts 
   end
 
   it 'responds with NO, NOT SINCE 1938! when she can hear you' do
